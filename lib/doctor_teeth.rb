10f09dc54111fc4c69e6bd935b1a02dbe988f180
Eric Thompson <eric.thompson@puppetlabs.com>
2017-10-30 15:09:48 -0700

    (QA-3045) QAELK REST service
    * standup sinatra and send data to the parser
    * do not attempt to cleanse or validate (this is bad)
    * do not ask for project/config/execution_id (this is bad)
    * remove local file reading (this is.. fine)

--- lib/doctor_teeth.rb
+++ lib/doctor_teeth.rb
@@ -1,5 +1,37 @@
 # frozen_string_literal: true
 
+require 'sinatra'
+require 'github/markdown'
+
+get '/' do
+  "This is the doctor_teeth ETL server, i know you are excited!<br>" +
+    "We accept junit.xml files on /junit<br>" +
+    "There are optional query parameters for 'project', 'configuration' and 'execution_id'<br>" +
+    '<p>' +
+    GitHub::Markdown.render_gfm(File.read(File.join('README.md')))
+end
+
+# test this with:
+#   curl -X POST --header "Content-Type: text/xml" --data-binary @fixtures/beaker_junit_01.xml localhost:4567/junit
+#   note the header is required so sinatra doesn't try to parse form params out of the data
+post '/junit' do
+  #project       = params['project']
+  #configuration = params['configuration']
+  #execution_id  = params['execution_id']
+
+  request.body.rewind  # in case someone already read it
+  datablob = request.body.read
+
+  #DoctorTeeth.parse(datablob,
+                    #{:configuration => configuration,
+                     #:project => project,
+                     #:execution_id => execution_id})
+  DoctorTeeth.parse(datablob,
+                    {:configuration => {}})
+
+
+end
+
 # the whole shebang
 module DoctorTeeth
   require 'doctor_teeth/parser'
