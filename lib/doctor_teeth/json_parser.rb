0639828c29c9c9ab85fd3c316d3e49d164e2f67a
Eric Thompson <eric.thompson@puppetlabs.com>
2017-11-09 13:34:26 -0800

    (QA-3196) fix rubocop errors or ignore them for now

--- lib/doctor_teeth/json_parser.rb
+++ lib/doctor_teeth/json_parser.rb
@@ -1,22 +1,23 @@
 # frozen_string_literal: true
 
-require 'nokogiri'
-require 'json'
-require 'time'
+require "nokogiri"
+require "json"
+require "time"
 module DoctorTeeth
-
   # A json file parser that takes extra metadata from a test_suite/case
   #   metadata that is not always in junit.xml
   #
   # @since v0.0.1
   # @attr_reader test_runs [String] holds the test_run data
+  # FIXME
+  # rubocop:disable Metrics/ClassLength
   class NewLineJsonFileParser
-
     # Holds the test_run data after parsing the json
     #
     # @api public
     # @example
-    #   @test_runs[job_url] = {"execution_id"=>"https://jenkins-master-prod-1.delivery.puppetlabs.net/job/blah",
+    #   @test_runs[job_url] = {"execution_id"=>
+    #     "https://jenkins-master-prod-1.delivery.puppetlabs.net/job/blah",
     # @return a test_runs hash
     attr_reader :test_runs
 
@@ -24,14 +25,17 @@ module DoctorTeeth
     #
     # @api public
     # @example
-    #   DoctorTeeth::NewLineJsonFileParser.new('some_file.json')
+    #   DoctorTeeth::NewLineJsonFileParser.new("some_file.json")
+    # FIXME
+    # rubocop:disable Metrics/PerceivedComplexity, Metrics/MethodLength
+    # rubocop:disable Metrics/CyclomaticComplexity, Metrics/AbcSize
     def initialize(file)
       json_files = []
       @test_runs = {}
 
       if File.directory?(file)
         json_files = Dir.entries(file)
-        json_files.delete_if { |f| f.chars.first == '.' }
+        json_files.delete_if { |f| f.chars.first == "." }
         json_files.map! { |f| File.join(file, f) }
 
       else
@@ -40,72 +44,74 @@ module DoctorTeeth
 
       file_count = json_files.length
       puts "Attempting to process #{file_count} json files"
+      # FIXME
+      # rubocop:disable Metrics/BlockLength
       json_files.each do |json|
         # totally unclear what this is doing
         #   counting chars?  allocating buffer?
-        output_char = file_count % 50 == 0 ? ".\n" : '.'
+        output_char = (file_count % 50).zero? ? ".\n" : "."
 
         File.open(json).each do |line|
           # TODO: fix dirty data
           begin
             json_object = JSON.parse(line)
           rescue JSON::ParserError => e
-            raise ("invalid JSON in file '#{json}': #{e}")
+            raise "invalid JSON in file '#{json}': #{e}"
           end
           # translate elasticsearch record to QALEK2 schema
 
           # TODO: use single instance var, don't pass to other method
-          if !json_object['_source']
-            raise ("missing '_source' key in file '#{json}'")
+          unless json_object["_source"]
+            raise "missing '_source' key in file '#{json}'"
           end
-          ['jenkins_build_url',
-           'job_name',
-           'start_time',
-           'test_case_suite',
-           'test_case_name',
-           'test_case_status',
-           'test_case_time',
-           'pre_suite_time',
-           'tests_time',
-           'configs'].each do |field|
-             if !json_object['_source'][field]
-               raise ("missing #{field} key in '_source' field in file '#{json}'")
-             end
-           end
-          execution_id = json_object['_source']['jenkins_build_url']
-          project = json_object['_source']['job_name']
+          %w[jenkins_build_url
+             job_name
+             start_time
+             test_case_suite
+             test_case_name
+             test_case_status
+             test_case_time
+             pre_suite_time
+             tests_time
+             configs].each do |field|
+            unless json_object["_source"][field]
+              raise "missing #{field} key in '_source' field in file '#{json}'"
+            end
+          end
+          execution_id = json_object["_source"]["jenkins_build_url"]
+          project = json_object["_source"]["job_name"]
           configuration = []
-          start_time = json_object['_source']['start_time']
-          suite_name = json_object['_source']['test_case_suite']
-          suite_duration = json_object['_source']["#{json_object['_source']['test_case_suite']}_time"]
-          test_name = json_object['_source']['test_case_name']
-          test_status = json_object['_source']['test_case_status']
-          test_duration = json_object['_source']['test_case_time']
+          start_time = json_object["_source"]["start_time"]
+          suite_name = json_object["_source"]["test_case_suite"]
+          suite_duration = json_object["_source"]["#{suite_name}_time"]
+          test_name = json_object["_source"]["test_case_name"]
+          test_status = json_object["_source"]["test_case_status"]
+          test_duration = json_object["_source"]["test_case_time"]
 
           # configuration experiment
-          conf = json_object['_source']['configs']
+          conf = json_object["_source"]["configs"]
           conf.each { |k, v| configuration.push("#{k}=#{v}") } if conf
 
           @test_record = {
-            'execution_id' => execution_id,
+            "execution_id" => execution_id,
             # TODO: need to find a better way to translate project name
-            'project'         => project,
-            # 'duration'        => json_object['_source'][],
-            'configuration'   => configuration,
-            'start_time'      => (Time.parse(start_time).utc if start_time),
-            'suite_name'      => suite_name,
+            "project"         => project,
+            # "duration"        => json_object["_source"][],
+            "configuration"   => configuration,
+            "start_time"      => (Time.parse(start_time).utc if start_time),
+            "suite_name"      => suite_name,
 
             # need to look up which suite this is
-            'suite_duration'  => suite_duration,
-            'test_name'       => test_name,
-            'test_status'     => test_status,
-            'test_duration'   => test_duration
+            "suite_duration"  => suite_duration,
+            "test_name"       => test_name,
+            "test_status"     => test_status,
+            "test_duration"   => test_duration
           }
 
           # add up test_run duration, values could be nil
-          suite_durations = [json_object['_source']['pre_suite_time'],
-                             json_object['_source']['tests_time']].compact
-          @test_record['duration'] = suite_durations.inject(:+)
+          suite_durations = [json_object["_source"]["pre_suite_time"],
+                             json_object["_source"]["tests_time"]].compact
+          @test_record["duration"] = suite_durations.inject(:+)
 
           # do the thing
           insert_record(@test_record)
@@ -122,52 +128,56 @@ module DoctorTeeth
     # @api private
     # @return who cares, its private
     def insert_record(test_record = {})
-      id            = test_record['execution_id']
-      project       = test_record['project']
-      duration      = test_record['duration']
-      configuration = test_record['configuration']
-      start_time    = test_record['start_time']
+      id            = test_record["execution_id"]
+      project       = test_record["project"]
+      duration      = test_record["duration"]
+      configuration = test_record["configuration"]
+      start_time    = test_record["start_time"]
 
       # suite properties
-      suite_name    = test_record['suite_name']
-      suite_duration = test_record['suite_duration']
+      suite_name    = test_record["suite_name"]
+      suite_duration = test_record["suite_duration"]
 
       # test cae properties
-      test_name     = test_record['test_name']
-      test_status   = test_record['test_status']
-      test_duration = test_record['test_duration']
+      test_name     = test_record["test_name"]
+      test_status   = test_record["test_status"]
+      test_duration = test_record["test_duration"]
 
       # set keys conditionally one at a time
-      @test_runs[id]                  ||= { 'execution_id' => id }
-      @test_runs[id]['project']       ||= project
-      @test_runs[id]['duration']      ||= duration
-      @test_runs[id]['configuration'] ||= configuration
-      @test_runs[id]['start_time']    ||= start_time
-      @test_runs[id]['test_suites']   ||= []
+      @test_runs[id]                  ||= { "execution_id" => id }
+      @test_runs[id]["project"]       ||= project
+      @test_runs[id]["duration"]      ||= duration
+      @test_runs[id]["configuration"] ||= configuration
+      @test_runs[id]["start_time"]    ||= start_time
+      @test_runs[id]["test_suites"]   ||= []
 
-      test_case = { 'name' => test_name,
-                    'duration' => test_duration, 'status' => test_status }
-      test_suite = { 'name' => suite_name,
-                     'duration' => suite_duration, 'test_cases' => [test_case] }
+      test_case  = { "name" => test_name,
+                     "duration" => test_duration,
+                     "status" => test_status }
+      test_suite = { "name" => suite_name,
+                     "duration" => suite_duration,
+                     "test_cases" => [test_case] }
 
-      if @test_runs[id]['test_suites'].empty?
+      if @test_runs[id]["test_suites"].empty?
 
         # create the first
-        @test_runs[id]['test_suites'].push(test_suite)
+        @test_runs[id]["test_suites"].push(test_suite)
       # does suite exist?
-      elsif @test_runs[id]['test_suites'].any? { |suite| suite['name'] == suite_name }
-        @test_runs[id]['test_suites'].each do |suite|
-          next unless suite['name'] == suite_name
+      elsif @test_runs[id]["test_suites"]
+            .any? { |suite| suite["name"] == suite_name }
+        @test_runs[id]["test_suites"].each do |suite|
+          next unless suite["name"] == suite_name
           # create empty test_cases array if it does not exist
-          suite['test_cases'] ||= []
-          # check if this is a duplicate record (there should not be duplicate records)
-          raise 'CRAP! DUPLICATE RECORD!' if suite['test_cases'].any? { |test| test == test_name }
+          suite["test_cases"] ||= []
+          # check if this is a duplicate record
+          raise "CRAP! DUPLICATE RECORD!" if suite["test_cases"]
+                                             .any? { |test| test == test_name }
           # add test case
-          suite['test_cases'].push(test_case)
+          suite["test_cases"].push(test_case)
         end
       else
         # create the suite with test_case inside
-        @test_runs[id]['test_suites'].push(test_suite)
+        @test_runs[id]["test_suites"].push(test_suite)
       end
     end
     private :insert_record
@@ -176,17 +186,17 @@ module DoctorTeeth
     #
     # @api public
     # @example
-    #   json_file_parser.generate_new_line_delimited_json_file('somedir')
+    #   json_file_parser.generate_new_line_delimited_json_file("somedir")
     # @return line_count?
     def generate_new_line_delimited_json_file(file)
       line_count = @test_runs.length
       puts "\nAttempting to write #{line_count} json objects to #{file}"
-      File.open(file, 'w') do |f|
+      File.open(file, "w") do |f|
         @test_runs.each do |_k, v|
-          output_char = line_count % 50 == 0 ? ".\n" : '.'
+          output_char = (line_count % 50).zero? ? ".\n" : "."
           print output_char
 
-          f.write(JSON.generate('test_run' => v))
+          f.write(JSON.generate("test_run" => v))
           f.write("\n")
 
           line_count += -1
@@ -198,7 +208,8 @@ module DoctorTeeth
     #
     # @api public
     # @example
-    #   json_file_parser.generate_new_line_delimited_json_files('somedir',number_of_files_to_generate)
+    #   json_file_parser.generate_new_line_delimited_json_files("somedir",
+    #     number_of_files_to_generate)
     # @return line_count?
     def generate_new_line_delimited_json_files(dir, number_of_desired_files)
       line_count = @test_runs.length
@@ -206,16 +217,20 @@ module DoctorTeeth
         "to files in the directory #{dir}"
 
       slice_count = 0
-      raise "too few test runs to split to #{number_of_desired_files} files" unless @test_runs.length >= number_of_desired_files
-      @test_runs.each_slice(@test_runs.length / number_of_desired_files) do |slice|
+      unless @test_runs.length >= number_of_desired_files
+        raise "too few runs to split: #{number_of_desired_files} files"
+      end
+
+      @test_runs.each_slice(@test_runs.length /
+                            number_of_desired_files) do |slice|
         slice_count += 1
 
-        File.open("#{dir}/file#{slice_count}", 'w') do |f|
+        File.open("#{dir}/file#{slice_count}", "w") do |f|
           slice.each do |_k, v|
-            output_char = line_count % 50 == 0 ? ".\n" : '.'
+            output_char = (line_count % 50).zero? ? ".\n" : "."
             print output_char
 
-            f.write(JSON.generate('test_run' => v))
+            f.write(JSON.generate("test_run" => v))
             f.write("\n")
 
             line_count += -1
