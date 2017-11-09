282ae3faca53f9498f3a196dde05318616774076
Eric Thompson <eric.thompson@puppetlabs.com>
2017-11-09 10:51:53 -0800

    (QA-3197) QAELK fix yard docs for the json parser and main app file

--- lib/doctor_teeth.rb
+++ lib/doctor_teeth.rb
@@ -38,16 +38,22 @@ module DoctorTeeth
   require 'doctor_teeth/json_parser'
 
   # method stub for parsing
-  # @param [String] path of junit xml file(s) of test_run data
-  # @param [Hash] opts the options containing extra metadata not in junit.xml
+  # @param xml [String] junit xml test_run data
+  # @param opts [Hash] the options containing extra metadata not in junit.xml
   # @return test_run
+  # @api public
+  # @example
+  #   DoctorTeeth.parse(<xml blah>, {:configuration => {}})
   def self.parse(xml, opts)
     DoctorTeeth::Parser.new(xml, opts).test_run
   end
 
   # method stub for parsing json
-  # @param [String] path of json file(s) containing indexed test_run data
+  # @param json [String] path of json file(s) containing indexed test_run data
   # @return test_run
+  # @api public
+  # @example
+  #   DoctorTeeth.parse_json_file('some_json.json')
   def self.parse_json_file(json)
     # @todo: make file????
     DoctorTeeth::NewLineJsonFileParser.new(json)
